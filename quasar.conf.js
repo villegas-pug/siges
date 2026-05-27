1b34b6421223c451cbf2c5a68c9eb0b99900e616
villegas-pug <villegas-pug@gmail.com>
2026-05-27 16:29:46 -0500

    Allow MPEG audio uploads in ficha

--- quasar.conf.js
+++ quasar.conf.js
@@ -48,6 +48,7 @@ module.exports = function (ctx) {
         // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
         build: {
             vueRouterMode: 'hash',
+            publicPath: '/siges/',
 
             env: {
                 API_URL : ctx.dev
