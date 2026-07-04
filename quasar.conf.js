8a0b871900e46be81e2872b26e7904831bb4e75b
villegas-pug <villegas-pug@gmail.com>
2026-07-03 23:57:53 -0500

    chore: update packagejson

--- quasar.conf.js
+++ quasar.conf.js
@@ -58,10 +58,10 @@ module.exports = function (ctx) {
 
                 // PROD
                 // API_URL_SIGESU: 'http://172.19.0.44:4000/api/ms-sigesu',
-                // API_URL_SIGESU: 'https://srvapp03.inabif.gob.pe:4000/api/ms-sigesu',
+                API_URL_SIGESU: 'https://srvapp03.inabif.gob.pe/api/ms-sigesu',
                 
                 // DEV
-                API_URL_SIGESU: 'http://10.101.0.36:4000/api/ms-sigesu',
+                // API_URL_SIGESU: 'http://10.101.0.36:4000/api/ms-sigesu',
                 API_RUTA:'https://srvapp01.inabif.gob.pe:8443/SRH',
 
                 TOKEN_EXPIRATION_MINUTES: 240,
